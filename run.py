9a1655802ceb509c40e6d262386743a49b0e85ef
pgk95 <kalupgk@gmail.com>
2024-01-03 19:45:26 +0300

    fixed some issues related to TLS and SSL

--- run.py
+++ run.py
@@ -10,7 +10,6 @@ load_dotenv()
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
 app.config['MAIL_PORT'] = os.getenv('MAIL_PORT')
 app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS')
-app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL')
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
@@ -20,12 +19,17 @@ app.config['MAIL_ASCII_ATTACHEMENTS'] = os.getenv('MAIL_ASCII_ATTACHEMENTS')
 mail = Mail(app)
 mail.init_app(app)
 
+
 @app.route('/')
 def index():
-    msg = Message('Hello', recipients=['<insert-your-receiver-email>'])
-    msg.body = 'Hello Flask message sent from Flask-Mail'
-    mail.send(msg)
-    return 'Message Sent'
+    try:
+        msg = Message('Hello', recipients=['yasajem138@wikfee.com'])
+        msg.body = 'Hello Flask message sent from Flask-Mail'
+        mail.send(msg)
+        return 'Message Sent! It WORKS.'
+    except Exception as e:
+        return str(e)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
